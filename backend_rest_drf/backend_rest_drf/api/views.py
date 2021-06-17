ca44bc484fae7b808ff8e84207a52f7b7420b43d
KingOnHuy <kingponpawit@gmail.com>
2021-06-17 19:01:59 +0700

    Add category

--- backend_rest_drf/backend_rest_drf/api/views.py
+++ backend_rest_drf/backend_rest_drf/api/views.py
@@ -1,7 +1,13 @@
-from store.models import Store
+from store.models import Store, Category
 from rest_framework import viewsets
-from store.serializers import StoreSerializer
+from store.serializers import StoreSerializer, CategorySerializer
+
 
 class StoreViewSet(viewsets.ModelViewSet):
     serializer_class = StoreSerializer
     queryset = Store.objects.all()
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category
--- backend_rest_drf/backend_rest_drf/store/serializers.py
+++ backend_rest_drf/backend_rest_drf/store/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import *
 
+
 class StoreSerializer(serializers.ModelSerializer):
     class Meta:
         fields = "__all__"
         model = Store
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = "__all__"
+        model = Category
