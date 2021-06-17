d76b24a298dd274d0f3021b19f2d9d938cd2fedb
KingOnHuy <kingponpawit@gmail.com>
2021-06-17 19:07:40 +0700

    Add Menu

--- backend_rest_drf/backend_rest_drf/api/views.py
+++ backend_rest_drf/backend_rest_drf/api/views.py
@@ -1,6 +1,6 @@
-from store.models import Store, Category
+from store.models import Store, Category, Menu
 from rest_framework import viewsets
-from store.serializers import StoreSerializer, CategorySerializer
+from store.serializers import StoreSerializer, CategorySerializer, MenuSerializer
 
 
 class StoreViewSet(viewsets.ModelViewSet):
@@ -11,3 +11,8 @@ class StoreViewSet(viewsets.ModelViewSet):
 class CategoryViewSet(viewsets.ModelViewSet):
     serializer_class = CategorySerializer
     queryset = Category
+
+
+class MenuViewSet(viewsets.ModelViewSet):
+    serializer_class = MenuSerializer
+    queryset = Menu
--- backend_rest_drf/backend_rest_drf/store/models.py
+++ backend_rest_drf/backend_rest_drf/store/models.py
@@ -3,13 +3,24 @@ from django.utils.translation import gettext_lazy as _
 from django.db import models
 import uuid
 
+
 class Store(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(_("Name"), max_length=50)
     description = models.TextField(_("Description"))
     rating = models.PositiveSmallIntegerField(_("Rating"))
 
+
 class Category(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    storeId = models.ForeignKey(Store, verbose_name=_("Store"), on_delete=models.CASCADE)
+    storeId = models.ForeignKey(Store, verbose_name=_(
+        "Store"), on_delete=models.CASCADE)
     name = models.CharField(_("Name"), max_length=50)
+
+
+class Menu(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    categoryId = models.ForeignKey(Category, verbose_name=_(
+        "Category"), on_delete=models.CASCADE)
+    name = models.CharField(_("Name"), max_length=50)
+    price = models.PositiveIntegerField(_("Price"))
