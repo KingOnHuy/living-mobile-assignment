7d79cca04c0f07d6857132e304b21cd38604a391
KingOnHuy <kingponpawit@gmail.com>
2021-06-18 13:29:33 +0700

    Update models string for admin

--- backend_rest_drf/backend_rest_drf/store/models.py
+++ backend_rest_drf/backend_rest_drf/store/models.py
@@ -10,12 +10,18 @@ class Store(models.Model):
     description = models.TextField(_("Description"))
     rating = models.PositiveSmallIntegerField(_("Rating"))
 
+    def __str__(self):
+        return f'{self.name} - {self.id}'
+
 
 class Category(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     storeId = models.ForeignKey(Store, verbose_name=_(
         "Store"), on_delete=models.CASCADE)
     name = models.CharField(_("Name"), max_length=50)
+
+    def __str__(self):
+        return f'{self.name} - {self.id}'
 
 
 class Menu(models.Model):
@@ -24,3 +30,6 @@ class Menu(models.Model):
         "Category"), on_delete=models.CASCADE)
     name = models.CharField(_("Name"), max_length=50)
     price = models.PositiveIntegerField(_("Price"))
+
+    def __str__(self):
+        return f'{self.name} - {self.id}'
