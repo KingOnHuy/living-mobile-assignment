2431fb9556cc64438ed6805f7229a461a0ae1574
KingOnHuy <kingponpawit@gmail.com>
2021-06-28 15:18:13 +0700

    Update Navbar image

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,22 +1,46 @@
 <template>
   <div>
     <el-menu
+      :router="true"
       :default-active="activeIndex"
       mode="horizontal"
       @select="handleSelect"
     >
-      <el-menu-item index="1">Story</el-menu-item>
-      <el-menu-item index="2">Category</el-menu-item>
-      <el-menu-item index="3">Menu</el-menu-item>
+      <el-menu-item class="d-flex justify-center align-center" index="">
+        <el-image
+          class="d-flex"
+          :src="require('@/assets/FoodStory_Logo.png')"
+          :fit="contain"
+        ></el-image>
+      </el-menu-item>
+      <el-menu-item index="store">Story</el-menu-item>
+      <el-menu-item index="category">Category</el-menu-item>
+      <el-menu-item index="menu">Menu</el-menu-item>
     </el-menu>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      activeIndex: "store",
+    };
+  },
+  methods: {
+    handleSelect(n) {
+      console.log(n);
+    },
+  },
+  mounted() {},
+};
 </script>
 
 <style>
+.el-menu {
+  height: 60px;
+}
+
 .el-menu-item {
   color: white !important;
 }
