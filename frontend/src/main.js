26239055e036bc4df119b750cf2c94fe6db2250e
KingOnHuy <kingponpawit@gmail.com>
2021-06-28 12:41:39 +0700

    Add main css

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import "./plugins/elements";
+import "./style/main.css";
 
 Vue.config.productionTip = false;
 
