5b5cd0da96b4bcb8fdbc3424062be7f48ad7a8af
KingOnHuy <kingponpawit@gmail.com>
2021-06-28 13:46:40 +0700

    Update navbar style

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -2,27 +2,12 @@
   <div>
     <el-menu
       :default-active="activeIndex"
-      class="el-menu-demo"
       mode="horizontal"
       @select="handleSelect"
     >
-      <el-menu-item index="1">Processing Center</el-menu-item>
-      <el-submenu index="2">
-        <template slot="title">Workspace</template>
-        <el-menu-item index="2-1">item one</el-menu-item>
-        <el-menu-item index="2-2">item two</el-menu-item>
-        <el-menu-item index="2-3">item three</el-menu-item>
-        <el-submenu index="2-4">
-          <template slot="title">item four</template>
-          <el-menu-item index="2-4-1">item one</el-menu-item>
-          <el-menu-item index="2-4-2">item two</el-menu-item>
-          <el-menu-item index="2-4-3">item three</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-      <el-menu-item index="3" disabled>Info</el-menu-item>
-      <el-menu-item index="4"
-        ><a href="https://www.ele.me" target="_blank">Orders</a></el-menu-item
-      >
+      <el-menu-item index="1">Story</el-menu-item>
+      <el-menu-item index="2">Category</el-menu-item>
+      <el-menu-item index="3">Menu</el-menu-item>
     </el-menu>
   </div>
 </template>
@@ -32,4 +17,22 @@ export default {};
 </script>
 
 <style>
+.el-menu-item {
+  color: white !important;
+}
+
+.el-menu-item:hover {
+  color: var(--primary) !important;
+  background-color: #86d5fc;
+}
+
+.el-menu-item:focus {
+  color: white !important;
+  background-color: #86d5fc !important;
+}
+
+.is-active {
+  border-bottom: 2px solid #00aaff !important;
+  background-color: #86d5fc;
+}
 </style>
