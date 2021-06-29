2f16ec3fd9205e479365d0e6b885427c9063a646
KingOnHuy <kingponpawit@gmail.com>
2021-06-29 16:05:56 +0700

    Update element locale

--- frontend/src/plugins/elements.js
+++ frontend/src/plugins/elements.js
@@ -2,5 +2,6 @@ import Vue from "vue";
 import ElementUI from "element-ui";
 // import "element-ui/lib/theme-chalk/index.css";
 import '../style/element-variables.scss';
+import locale from 'element-ui/lib/locale/lang/en'
 
-Vue.use(ElementUI);
+Vue.use(ElementUI, { locale });
